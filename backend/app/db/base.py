28445f9ffb701812e6459ac3fd422da8e52d9d2b
Bernardo <bernardo@MacBook-Pro-de-Bernardo.local>
2025-10-07 19:51:51 -0600

    chore(db): ensure base imports for Alembic

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -1,3 +1,4 @@
-from ..models import Appointment, Base, Client, User
-
-__all__ = ["Base", "User", "Client", "Appointment"]
+from app.db.base_class import Base
+from app.models.client import Client  # noqa
+from app.models.appointment import Appointment  # noqa
+from app.models.user import User  # noqa
